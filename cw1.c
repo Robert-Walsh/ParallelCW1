5e3a7684688b39e74e71f2a9e6ef3fa5685f57f5
Robert-Walsh <rw539@bath.ac.uk>
2016-11-17 22:19:30 +0000

    Working solution, needs cleaning

--- cw1.c
+++ cw1.c
@@ -7,12 +7,9 @@
 #define ARRAY2DLOOKUP(SquareArray, Dim, X, Y) (SquareArray)[(Y)*(Dim) + (X)]
 
 pthread_mutex_t printLock;
-pthread_barrier_t barrierTest;
-
+pthread_barrier_t barrierEndWhile;
 pthread_barrier_t barrierEnterLoop;
-pthread_barrier_t barrierEndLoop;
-
-pthread_mutex_t signalEndMutex;
+pthread_barrier_t barrierEndForLoop;
 
 int fUnderPrecision = 1;
 int fSignalEnd = 0;
@@ -24,19 +21,6 @@ struct segment_LinkedListNode {
 	struct segment_LinkedListNode* next;
 };
 
-void deleteArgsLinkedList(struct segment_LinkedListNode* args) {
-	if (args != NULL) {
-
-		if (args->next != NULL) {
-			deleteArgsLinkedList(args->next);
-		}
-		args->next = NULL;
-		args->segmentStart = NULL;
-		args->segmentLength = 0;
-	}
-}
-
-
 struct arg_struct {
 	struct segment_LinkedListNode linkedListStart;
 
@@ -44,48 +28,53 @@ struct arg_struct {
 	int totalSquareArrayDimensions;
 };
 
-void deleteArgs(struct arg_struct* args) {
-	deleteArgsLinkedList(&args->linkedListStart);
-	args->precision = 0.0;
-	args->totalSquareArrayDimensions = 0;
-}
 void printArray(double* squareArray, int dim);
-
 void* arrayManipulator(void* args);
+void deleteArgs(struct arg_struct* args);
+void deleteArgsLinkedList(struct segment_LinkedListNode* args);
 
+/*
+*    This function organises the values pointed to by the squareArray 
+*    into segments based on the number of threads and the number of calculatable values.
+*    It creates arguments containing relevant information that each thread needs. 
+*
+*    \param    squareArray    A pointer to a square multidimensional array
+*    \param    dim            Dimensions of the square multidimensional array. The width and height of the square               
+*                             multidimensional array.
+*    \param    precision      The solver will finish when the values per iteration in total change less than or equal to this   
+*                             value
+*    \param    nThreads       The number of threads used to solve the problem. 
+*
+*    \returns  0 when did not finish successfully and 1 when it does finish successfully.
+*/
 int solver(double* squareArray, int dim, double precision, int nThreads) {
 
+    //calculate number of values that will be modified
     int rows = dim-2;
     int numberOfNumbers = rows*rows;
-
-    double largestChange;
-    double ABSed;
-
-    //set number of threads as the max amount of rows we can work on at any time
-    // if(nThreads>((dim-1)/2)){
-    //     nThreads = (dim-1)/2;
-    // }
+    
     pthread_t threads[nThreads];
-    struct arg_struct arguments[nThreads]; //make an array of structs
+    struct arg_struct arguments[nThreads]; 
 
-    pthread_barrier_init(&barrierTest, NULL, nThreads);
+    //Initialise our barrier used in arrayManipulator
+    pthread_barrier_init(&barrierEndWhile, NULL, nThreads);
     pthread_barrier_init(&barrierEnterLoop, NULL, nThreads);
-    pthread_barrier_init(&barrierEndLoop, NULL, nThreads);
+    pthread_barrier_init(&barrierEndForLoop, NULL, nThreads);
 
-    // printf("%d", NUM_OF_THREADS);
-    // printf("hello");
-
-
+    //Initialise flags
     fUnderPrecision = 1;
     fSignalEnd = 0;
+   
     int i=0;
-    //do{
-
+    
+    //Calculate number of values for each thread, and the remainder
     int totalData = (dim-2)*(dim-2);
     int sizePerThread = totalData/nThreads;
     int remainder = totalData%nThreads;
     int currentIt = dim+1;
     int rowEnd = dim*2 - 1;
+    
+    //Here we create the segments for each thread
     for(i=0; i<nThreads; ++i){
         if ((currentIt + 1) % dim == 1) {
 			++currentIt;
@@ -120,7 +109,6 @@ int solver(double* squareArray, int dim, double precision, int nThreads) {
 				}
 
 				linkedListIterator->next = (struct segment_LinkedListNode*)malloc(sizeof(struct segment_LinkedListNode));
-
 				linkedListIterator = linkedListIterator->next;
 				linkedListIterator->next = NULL;
 
@@ -129,14 +117,11 @@ int solver(double* squareArray, int dim, double precision, int nThreads) {
 				}
 
 				linkedListIterator->segmentStart = squareArray + (++currentIt);
-
-
 			}
 			else {
 				--sizeCountdown;
 				++currentIt;
 			}
-
 		}
 
         pthread_create(&threads[i], NULL, arrayManipulator, (void *)&arguments[i]);
@@ -149,12 +134,10 @@ int solver(double* squareArray, int dim, double precision, int nThreads) {
         //deleteArgs(&arguments[i]);
     }
 
-    fSignalEnd = 0;
-
-   // }
-    //while(1==0);//largestChange<precision);
-
-
+    //reset state of fSignalEnd
+    fSignalEnd = 0; 
+    
+    return 0;
 }
 
 
@@ -168,14 +151,12 @@ void* arrayManipulator(void* arguments){
     int dim = args->totalSquareArrayDimensions;
     double precision = args->precision;
 
-    int j,iteration;
+    int j, iteration;
     iteration = 0;
     do{
-        printf("%d is SETTING UNDER PRECISION as 1 \n", (int)pthread_self());
         fUnderPrecision = 1;
 
         pthread_barrier_wait (&barrierEnterLoop);
-        printf("%d thread iteration %d\n", (int)pthread_self(), iteration);
         for(linkedListIterator = &args->linkedListStart; linkedListIterator!=NULL;linkedListIterator = linkedListIterator->next ){
             for(j = 0; j < linkedListIterator->segmentLength; ++j){
                 double prevIteration = (linkedListIterator->segmentStart)[j];
@@ -185,25 +166,17 @@ void* arrayManipulator(void* arguments){
                                                         + (linkedListIterator->segmentStart)[j+dim]
                                                       ) / 4.0;
 
-                printf("%d SEES UNDER PRECISION as %d \n", (int)pthread_self(), fUnderPrecision);
                 if(fUnderPrecision == 1 && fabs((linkedListIterator->segmentStart)[j]-prevIteration) > precision){
-                    printf("%d is SETTING UNDER PRECISION as 0 \n", (int)pthread_self());
                     fUnderPrecision = 0;
                 }
-
             }
         }
 
-        pthread_barrier_wait (&barrierEndLoop);
-        printf("%d is LOCKED\n", (int)pthread_self());
-        pthread_mutex_lock(&signalEndMutex);
+        pthread_barrier_wait (&barrierEndForLoop);
         if(fSignalEnd == 0 && fUnderPrecision == 1){
-            printf("%d is SETTING SIGNAL END as 1 \n", (int)pthread_self());
             fSignalEnd = 1;
         }
-
-        pthread_mutex_unlock(&signalEndMutex);
-        pthread_barrier_wait (&barrierTest);
+        pthread_barrier_wait (&barrierEndWhile);
 
         ++iteration;
     } while(fSignalEnd == 0);
@@ -215,6 +188,7 @@ int main(int argc, char *argv[]) {
     static const int dimension = 5;
     static const int numberOfThreads = 2;
     static const double precision = 0.001; //the precision to average the numbers to
+    
     /// Square multidimensional array in row-column format. E.g. data[1][2] looks up x=2, y=1
     double data[5][5] =   {
                             {1.0,2.0,0.5,3.0,2.2},
@@ -224,37 +198,48 @@ int main(int argc, char *argv[]) {
                             {1.3,0.1,0.7,2.0,2.1},
                         };
 
-
-
-
     if(!dimension>2){
         printf("Array size not large enough");
         return 0;
     }
 
-
     pthread_mutex_init(&printLock, NULL);
-    pthread_mutex_init(&signalEndMutex, NULL);
 
     printf("\nBefore Solver:\n");
     printArray(&data[0][0], dimension);
     printf("\n\n");
 
-
     solver(&data[0][0], dimension, precision, numberOfThreads);
-
 
     printf("After Solver:\n");
     printArray(&data[0][0], dimension);
     printf("\n\n");
 
     pthread_mutex_destroy(&printLock);
-    pthread_mutex_destroy(&signalEndMutex);
-    return 0;    /// Success
+    return 0;   
 }
 
-void printArray(double* squareArray, int dim){
+void deleteArgs(struct arg_struct* args) {
+	deleteArgsLinkedList(&args->linkedListStart);
+	args->precision = 0.0;
+	args->totalSquareArrayDimensions = 0;
+}
 
+
+void deleteArgsLinkedList(struct segment_LinkedListNode* args) {
+	if (args != NULL) {
+
+		if (args->next != NULL) {
+			deleteArgsLinkedList(args->next);
+		}
+		args->next = NULL;
+		args->segmentStart = NULL;
+		args->segmentLength = 0;
+	}
+}
+
+
+void printArray(double* squareArray, int dim){
     pthread_mutex_lock(&printLock);
 
     int y,x;
